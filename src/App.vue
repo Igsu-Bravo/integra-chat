cbfd344cbb0c7408c18307929a6784b219b28187
igsu-bravo <juan.bravo@futurice.com>
2019-11-14 07:10:07 +0200

    Removed unnecessary components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Integra-chat</h1>
+    <Chat />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Chat from "./containers/Chat.vue"
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Chat
   }
 }
 </script>
---
+++ src/containers/Chat.vue
@@ -0,0 +1,16 @@
+<template>
+    <p>Placeholder for chat main container</p>
+</template>
+<script>
+export default {
+    name: 'Chat',
+    props: {
+        conf: [],
+    }
+}
+</script>
+<style scoped>
+p {
+    font-family: 'Courier New', Courier, monospace;
+}
+</style>
